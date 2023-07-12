248974e409ecac1bd19dc8d9424037d23325fd3c
Sandra <sandramtzd@hotmail.com>
2023-07-12 16:07:13 +0100

    added properties to class

---
+++ BankAccount/src/test/java/BankAccountTest.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class BankAccountTest {
+
+    String firstName;
+    String lastName;
+    LocalDate dateOfBirth;
+    int accountNumber = 0;
+
+    int balance = 0;
+}
